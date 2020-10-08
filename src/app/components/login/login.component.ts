26b298e06338fa0718e225037676e8b837bbaf9d
Alok <alokpatel863@gmail.com>
2020-10-08 13:36:21 +0530

    add client side validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: [''],
+  });
 
-  constructor() { }
+  errorMessage = {
+    emailError: '',
+    passwordError: '',
+  };
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.loginForm.status !== 'INVALID') {
+      // API code
+    } else {
+      if (this.loginForm.controls.email.value === '') {
+        this.errorMessage.emailError = 'This field is required';
+      } else {
+        if (this.loginForm.controls.email.status === 'INVALID') {
+          this.errorMessage.emailError = 'Invalid Email';
+        }
+      }
+
+      if (this.loginForm.controls.password.value === '') {
+        this.errorMessage.passwordError = 'This field is required';
+      }
+    }
+    console.log(this.loginForm.value);
+    console.log('email', this.loginForm.controls.email.status);
   }
-
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -10,14 +10,58 @@ export class SignupComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
   signupForm = this.fb.group({
     userName: ['', Validators.required],
-    email: ['', Validators.email, Validators.required],
-    password: ['',Validators.required],
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', Validators.required],
     confirmPassword: [''],
   });
+
+  errorMessage = {
+    userNameError: '',
+    emailError: '',
+    passwordError: '',
+    confirmPasswordError: '',
+  };
 
   ngOnInit(): void {}
 
   onSubmit() {
-    console.log('value', this.signupForm);
+    if (this.signupForm.status !== 'INVALID') {
+      // handel confirm password validton if form is valid
+      if (
+        this.signupForm.controls.confirmPassword.value !==
+        this.signupForm.controls.password.value
+      ) {
+        this.errorMessage.confirmPasswordError = "Password doesn't match";
+        this.errorMessage.passwordError = '';
+        this.errorMessage.userNameError = '';
+        this.errorMessage.emailError = '';
+      } else {
+        // submitForm function run if form is valid and password are same
+        this.submitForm(this.signupForm.value);
+      }
+      console.log('inside else', this.signupForm.status);
+    } else {
+      // blank input field handling code
+      if (this.signupForm.controls.userName.value === '') {
+        this.errorMessage.userNameError = 'This field is required';
+      }
+      if (this.signupForm.controls.email.value === '') {
+        this.errorMessage.emailError = 'This field is required';
+      } else {
+        if (this.signupForm.controls.email.status === 'INVALID') {
+          this.errorMessage.emailError = 'Invalid email';
+        }
+      }
+      if (this.signupForm.controls.password.value === '') {
+        this.errorMessage.passwordError = 'This field is required';
+      }
+      if (this.signupForm.controls.confirmPassword.value === '') {
+        this.errorMessage.confirmPasswordError = 'This field is required';
+      }
+    }
+  }
+
+  submitForm(userDetails) {
+    console.log('user details', userDetails);
   }
 }
